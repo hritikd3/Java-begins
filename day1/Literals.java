3a8714811be9748a6f5bd79cac06b404407ca455
hritikd3 <hritikdangi3@gmail.com>
2023-07-28 10:47:13 +0530

    literals explained

---
+++ day1/Literals.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+
+class Literals{
+    public static void main(String[] args) {
+           long l=9999999999L;
+        float f=12.56f;
+        double d=12.56d;
+        
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+    }
+}
