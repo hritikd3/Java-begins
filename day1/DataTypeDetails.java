c0b01ff4095fe9431ff4f2fd712584caa6232e00
hritikd3 <hritikdangi3@gmail.com>
2023-07-28 11:12:31 +0530

    check how -ve no stored

---
+++ day1/DataTypeDetails.java
@@ -0,0 +1,16 @@
+
+
+import java.awt.*;
+import javax.swing.*;
+import java.lang.*;
+
+
+ class DataTypeDetails {
+
+    public static void main(String[] args) {
+        int x=-5;//5
+        
+        System.out.println(Integer.toBinaryString(x));  //11111111111111111111111111111011
+    }
+    
+}
