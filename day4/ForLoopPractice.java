3f702255b0b9e345bc0d400c9f6df4bcecb68fcb
hritikd3 <hritikdangi3@gmail.com>
2023-08-03 09:21:42 +0530

    for loop in practice

---
+++ day4/ForLoopPractice.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class ForLoopPractice {
+ public static void main(String[] args) {
+     for(int q=0,t=1;q<=10  ;q++,t=t*2){  //here q is changing linearly and t is changing exponentially
+            System.out.println(q+ " " + t);
+         }
+ }   
+}
