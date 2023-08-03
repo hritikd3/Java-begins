aa641ceb309033a802491c7d5d0feff44cfc4d51
hritikd3 <hritikdangi3@gmail.com>
2023-08-03 10:11:54 +0530

    sum the num

--- day4/SumOfNum.java
+++ day4/SumOfNum.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 public class SumOfNum {
     public static void main(String[] args) {
-        
+        int i=1, n=10,sum=0;
+        for(  ; i<=n; i++){
+  sum += i;
+//   System.out.println(   sum);
+}
+System.out.println(   "Sum of number upto "+ n + " is :- " +sum);
     }
 }
