eb0422719389fdcda13703938be1e3f8e1a702fe
hritikd3 <hritikdangi3@gmail.com>
2023-08-03 09:21:10 +0530

    do while loop in practice

---
+++ day4/DoWhile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class DoWhile {
+    public static void main(String args[]) {
+        int i=1,n=10;
+        do{
+            System.out.println(i);
+  i=i*2;
+        }while(i<n);
+    }
+}
